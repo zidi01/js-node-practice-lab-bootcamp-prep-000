b89ab347886092db3850a2dd9819cbca1ca646b5
zidi <zidi01@ukr.net>
2018-04-27 13:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // write your code below!
 
 var name = "Zidi"
-var height = "65"
+var height = 65
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
 console.log(height)
-console.log(parseInt(height) + 1)
+//console.log(parseInt(height) + 1)
+console.log(height + 1)
